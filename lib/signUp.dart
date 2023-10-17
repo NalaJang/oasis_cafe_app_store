abd077bc5d01a728bbed24d26b0246a3ea277a4d
nala0897@naver.com <nala0897@naver.com>
2023-10-17 20:46:12 +1300

    Fix:signUp stateless -> stateful

--- lib/signUp.dart
+++ lib/signUp.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:oasis_cafe_app_store/strings/strings_en.dart';
 
-class SignUp extends StatelessWidget {
+class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
 
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
