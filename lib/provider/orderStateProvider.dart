ec359da572c9a369e793bb1ee066aacb04542953
nala0897@naver.com <nala0897@naver.com>
2023-10-26 19:01:44 +1300

    Add:주석 추가

--- lib/provider/orderStateProvider.dart
+++ lib/provider/orderStateProvider.dart
@@ -42,14 +42,14 @@ class OrderStateProvider with ChangeNotifier {
   // 주문 상태 업데이트
   Future<void> updateOrderState(int index, String orderId, String processState) async {
 
+    // '주문 접수' 클릭 시 '처리중' 상태로 업데이트
     if( processState == 'new' ) {
-
       await orderCollection.doc(orderId).update({
         'processState' : 'inProcess',
       });
 
+      // '처리중' 클릭 시 신규/처리중 항목에서 삭제 및 '완료' 상태로 업데이트
     } else if( processState == 'inProcess' ) {
-
       OrderModel.setDataToCompletedOrder(orderId, orderList[index]);
       await orderCollection.doc(orderId).delete();
     }
