5e4b4a09608338ec46010989f437d8ba9987cb4e
nala0897@naver.com <nala0897@naver.com>
2023-11-01 22:43:01 +1300

    Fix:영업시간 수정 페이지 UI

--- lib/screens/aboutUs/aboutUsEditPage.dart
+++ lib/screens/aboutUs/aboutUsEditPage.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:oasis_cafe_app_store/config/palette.dart';
 import 'package:oasis_cafe_app_store/model/model_openingHours.dart';
 
@@ -15,15 +15,27 @@ class _AboutUsEditPageState extends State<AboutUsEditPage> {
   var openingHoursModel = OpeningHoursModel();
   List<String> dayList = ['월', '화', '수', '목', '금', '토', '일'];
 
-  @override
-  void dispose() {
-    super.dispose();
+  DateTime _openTime = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day,
+      DateTime.now().hour, 00);
+  DateTime _closeTime = DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day,
+      DateTime.now().hour, 0);
 
-    // mondayOpenHour.dispose();
-    // mondayOpenMinutes.dispose();
-    // mondayCloseHour.dispose();
-    // mondayCloseMinutes.dispose();
 
+  Container setSelectedTime(DateTime time) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey, width: 1),
+          borderRadius: BorderRadius.circular(8.0)
+      ),
+      child: Text(
+        '${time.hour} : ${time.minute}',
+        style: const TextStyle(
+            fontSize: 18.0,
+            color: Colors.black87
+        ),
+      ),
+    );
   }
 
 
@@ -40,100 +52,42 @@ class _AboutUsEditPageState extends State<AboutUsEditPage> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              for( var i = 0; i < dayList.length; i++ )
               Row(
                 children: [
-                  Text(
-                    '${dayList[i]}요일',
-                    style: const TextStyle(
-                      fontSize: 17.0
-                    ),
-                  ),
-
-                  const SizedBox(width: 10,),
-                  Column(
-                    children: [
-                      Text(
-                        openingHoursModel.openAmPm[i],
-                        style: TextStyle(
-                          color: openingHoursModel.openAmPm[i] == '오전' ?
-                              Colors.blue : Colors.black54
-                        ),
-                      ),
-                      Text(
-                        '오후',
-                        style: TextStyle(
-                            color: openingHoursModel.openAmPm[i] == '오후' ?
-                            Colors.blue : Colors.black54
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  Expanded(
-                    child: TextFormField(
-                      controller: openingHoursModel.openHours[i],
-                      textAlign: TextAlign.center,
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                      maxLength: 2,
-                      decoration: const InputDecoration(
-                        counterText: ''
+                  const Text('평일(월~금)'),
+                  const Text('시작'),
+                  CupertinoButton(
+                    onPressed: () => _showTimePickerDialog(
+                      CupertinoDatePicker(
+                        initialDateTime: _openTime,
+                        mode: CupertinoDatePickerMode.time,
+                        use24hFormat: false,
+                        minuteInterval: 30,
+                        onDateTimeChanged: (DateTime newTime) {
+                          setState(() => _openTime = newTime);
+                        },
                       ),
                     ),
+
+                    child: setSelectedTime(_openTime),
                   ),
 
-                  const Text(':'),
-
-                  Expanded(
-                    child: TextFormField(
-                      controller: openingHoursModel.openMinutes[i],
-                      textAlign: TextAlign.center,
-                      // maxLength: 2,
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                      maxLength: 2,
-                      decoration: const InputDecoration(
-                          counterText: ''
+                  const Text('종료'),
+                  CupertinoButton(
+                    onPressed: () => _showTimePickerDialog(
+                      CupertinoDatePicker(
+                        initialDateTime: _closeTime,
+                        mode: CupertinoDatePickerMode.time,
+                        use24hFormat: true,
+                        minuteInterval: 30,
+                        onDateTimeChanged: (DateTime newTime) {
+                          setState(() => _closeTime = newTime);
+                        },
                       ),
                     ),
+
+                    child: setSelectedTime(_closeTime)
                   ),
-
-                  const SizedBox(width: 10,),
-                  const Text('~'),
-                  const SizedBox(width: 10,),
-
-                  const Text('오후'),
-                  const Text('오후'),
-
-                  Expanded(
-                    child: TextFormField(
-                      controller: openingHoursModel.closeHours[i],
-                      textAlign: TextAlign.center,
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                      maxLength: 2,
-                      decoration: const InputDecoration(
-                          counterText: ''
-                      ),
-                    ),
-                  ),
-
-                  const Text(':'),
-
-                  Expanded(
-                    child: TextFormField(
-                      controller: openingHoursModel.closeMinutes[i],
-                      textAlign: TextAlign.center,
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                      maxLength: 2,
-                      decoration: const InputDecoration(
-                          counterText: ''
-                      ),
-                    ),
-                  ),
-
                 ],
               ),
             ],
@@ -143,16 +97,26 @@ class _AboutUsEditPageState extends State<AboutUsEditPage> {
     );
   }
 
-  // void _showTimePicker(Widget chile) {
-  //   showCupertinoModalPopup(
-  //     context: context,
-  //     builder: (BuildContext context) => Container(
-  //       height: 200,
-  //       child: SafeArea(
-  //         top: false,
-  //         child: chile,
-  //       ),
-  //     )
-  //   );
-  // }
+
+  void _showTimePickerDialog(Widget child) {
+    showCupertinoModalPopup(
+      context: context,
+      builder: (BuildContext context) => Container(
+        height: 216,
+        padding: const EdgeInsets.only(top: 6.0),
+        // The Bottom margin is provided to align the popup above the system
+        // navigation bar.
+        margin: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        // Provide a background color for the popup.
+        color: CupertinoColors.systemBackground.resolveFrom(context),
+        // Use a SafeArea widget to avoid system overlaps.
+        child: SafeArea(
+          top: false,
+          child: child,
+        ),
+      ),
+    );
+  }
 }
