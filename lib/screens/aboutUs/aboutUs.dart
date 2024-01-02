d5f890391cc4bf78e7c90c0d985ac1d32061c2f4
nala0897@naver.com <nala0897@naver.com>
2024-01-02 19:41:03 +0900

    Fix:영업 시간 코드를 OpeningHours 클래스로 분리

--- lib/screens/aboutUs/aboutUs.dart
+++ lib/screens/aboutUs/aboutUs.dart
@@ -1,23 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:oasis_cafe_app_store/config/palette.dart';
-import 'package:oasis_cafe_app_store/screens/aboutUs/aboutUsEditPage.dart';
-import 'package:provider/provider.dart';
-
-import '../../provider/openingHoursProvider.dart';
+import 'package:oasis_cafe_app_store/screens/aboutUs/openingHours.dart';
 
 class AboutUs extends StatelessWidget {
   const AboutUs({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
-    List<String> dayList = ['월', '화', '수', '목', '금', '토', '일'];
-    var openingHoursProvider = Provider.of<OpeningHoursProvider>(context);
-    openingHoursProvider.getOpeningHours();
-
-    var dayTextSize = const TextStyle(fontSize: 17.0);
-
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('About us'),
@@ -28,64 +17,13 @@ class AboutUs extends StatelessWidget {
         padding: const EdgeInsets.all(10.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  '영업 시간',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
+          children: const [
 
-                GestureDetector(
-                  onTap: (){
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) =>
-                        const AboutUsEditPage())
-                    );
-                  },
-                  child: const Text(
-                    '수정하기',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 17.0
-                    ),
-                  ),
-                )
-              ],
-            ),
+            // 영업 시간
+            OpeningHours(),
 
-            const SizedBox(height: 10,),
+            Divider(height: 30, color: Colors.grey,)
 
-            for( var i = 0; i < dayList.length; i++ )
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '${dayList[i]}요일',
-                    style: dayTextSize,
-                  ),
-
-                  openingHoursProvider.hoursList.isEmpty ?
-                      const CircularProgressIndicator() :
-                      openingHoursProvider.hoursList[i].openHour == '0' ?
-                      Text(
-                        '휴무',
-                        style: dayTextSize,
-                      ) :
-                      Text(
-                        '${openingHoursProvider.hoursList[i].openAmPm} ${openingHoursProvider.hoursList[i].openHour}:${openingHoursProvider.hoursList[i].openMinutes} ~ '
-                            '${openingHoursProvider.hoursList[i].closeAmPm} ${openingHoursProvider.hoursList[i].closeHour}:${openingHoursProvider.hoursList[i].closeMinutes}',
-                        style: dayTextSize,
-                      )
-                ],
-              ),
-
-
-            const Divider(height: 10, color: Colors.grey,),
           ],
         ),
       ),
---
+++ lib/screens/aboutUs/openingHours.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../provider/openingHoursProvider.dart';
+import 'aboutUsEditPage.dart';
+
+class OpeningHours extends StatelessWidget {
+  const OpeningHours({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    List<String> dayList = ['월', '화', '수', '목', '금', '토', '일'];
+
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+
+            _title(),
+
+            // 수정하기 버튼
+            _editButton(context),
+          ],
+        ),
+
+        const SizedBox(height: 10,),
+
+        for( var i = 0; i < dayList.length; i++ )
+          _getOpeningHoursInfo(context, dayList[i], i)
+      ],
+    );
+  }
+
+  Widget _title() {
+    return const Text(
+      '영업 시간',
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold
+      ),
+    );
+  }
+
+  // 수정하기 버튼
+  Widget _editButton(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context,
+          MaterialPageRoute(builder: (context) =>
+          const AboutUsEditPage())
+        );
+      },
+      child: const Text(
+        '수정하기',
+        style: TextStyle(
+          color: Colors.blue,
+          fontSize: 17.0
+        ),
+      ),
+    );
+  }
+
+  Widget _getOpeningHoursInfo(BuildContext context, String day, int index) {
+    var dayTextSize = const TextStyle(fontSize: 17.0);
+    var openingHoursProvider = Provider.of<OpeningHoursProvider>(context);
+    openingHoursProvider.getOpeningHours();
+
+    String openAmPm = openingHoursProvider.hoursList[index].openAmPm;
+    String openHour = openingHoursProvider.hoursList[index].openHour;
+    String openMinutes = openingHoursProvider.hoursList[index].openMinutes;
+    String closeAmPm = openingHoursProvider.hoursList[index].closeAmPm;
+    String closeHour = openingHoursProvider.hoursList[index].closeHour;
+    String closeMinutes = openingHoursProvider.hoursList[index].closeMinutes;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          '$day요일',
+          style: dayTextSize,
+        ),
+
+        openingHoursProvider.hoursList.isEmpty ?
+        const CircularProgressIndicator() :
+        openingHoursProvider.hoursList[index].openHour == '0' ?
+        Text(
+          '휴무',
+          style: dayTextSize,
+        ) :
+        Text(
+          '$openAmPm $openHour:$openMinutes ~ $closeAmPm $closeHour:$closeMinutes',
+          style: dayTextSize,
+        )
+      ],
+    );
+  }
+}
